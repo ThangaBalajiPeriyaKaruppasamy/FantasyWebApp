4308895b302b45169b3c9f4d1f73a027e85ba0d5
ThangaBalajiPeriyaKaruppasamy <thangabalajip@gmail.com>
2025-08-25 11:03:54 +0530

    main

--- FantasyWebApp/Program.cs
+++ FantasyWebApp/Program.cs
@@ -1,25 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
 
-app.UseAuthorization();
-
 app.MapRazorPages();
-
 app.Run();
